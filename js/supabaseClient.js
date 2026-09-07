b777ac1abb653fe09de0d8973ee28888bdeaae1b
RKatzYGN <rabbimkatz@yeshivaofgreatneck.org>
2026-09-06 20:18:36 -0400

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,5 @@
+// js/supabaseClient.js
+const SUPABASE_URL = 'https://YOUR_SUPABASE_PROJECT_ID.supabase.co';
+const SUPABASE_KEY = 'YOUR_SUPABASE_ANON_KEY';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
