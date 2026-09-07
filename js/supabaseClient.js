e94f8529d93688a1b75b8e01d79b05b56c7e87f3
RKatzYGN <rabbimkatz@yeshivaofgreatneck.org>
2026-09-06 21:15:24 -0400

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // js/supabaseClient.js
-const SUPABASE_URL = 'https://qvdcddcuscqooydyahba.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://qvdcddcuscqooydyahba.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2ZGNkZGN1c2Nxb295ZHlhaGJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3MzExMzksImV4cCI6MjEwNDMwNzEzOX0.eJVbWtp011v4jA_gkW_aj0TMj9Z5ewuujh4pVmK5ieQ';
 
 // Use window.supabase.createClient and assign it to a distinct variable name
